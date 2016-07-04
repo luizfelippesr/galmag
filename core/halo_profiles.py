4ad5907c4e57802ca8c02be3029126bb4f8cee19
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-07-04 18:09:37 +0100

    Simplifies the halo rotation curve and corrects its calling.

--- core/halo_profiles.py
+++ core/halo_profiles.py
@@ -1,27 +1,31 @@
-""" Contains the definitions of the halo rotation curve and alpha profile. """
+""" This module is part of GMF tool
+
+    Contains the definitions of the halo rotation curve and alpha profile.
+"""
 import numpy as N
 
-def simple_V(r_sph, R_h=1.0, V0=1.0, s0=5.0, R_ref=10.0):
+def simple_V(r_sph, R_h=1.0, V0=1.0, s0=0.5):
     """ Simple form of the rotation curve to be used for the halo
+        NB This simple form has no z dependence
+        V(r,theta,phi) = V0 (1-exp(-r sin(theta) / s0))
         Input: r_sph -> 3xNxNxN grid in spherical coordinates
-               Rref -> Radius of the reference halo. Default: 10 kpc
-               s0 -> s0 parameter in the reference halo. Default: 5 kpc
-               R -> Radius of the halo in the units of r_sph. Default: 1
-               V0 -> Normalization of the rotation curve. Default: 1
+               s0 -> s0 parameter Default: 0.5
+               R_h -> unit of r_sph in kpc [e.g. R_d=(halo radius in kpc)
+                      for r=0..1 within the halo]. Default: 1.0
+               V0 -> Normalization of the rotation curve. Default: 1.0
         Ouput: V -> rotation curve in the units of V0
     """
-    s0 = s0/R_ref * R_h
 
     V = N.zeros_like(r_sph)
     rho = r_sph[0,:,:,:]
     theta = r_sph[1,:,:,:]
 
-    V[2,:,:,:] = V0 * (1.0 - N.exp(-rho*N.sin(theta)/s0))
+    V[2,:,:,:] = V0 * (1.0 - N.exp(-rho*N.sin(theta)/s0/R_h))
 
     return V
 
 
-def simple_alpha(r, alpha0=1):
+def simple_alpha(r, alpha0=1.0):
     """ Simple profile for alpha"""
     rho = r[0,:,:,:]
     theta = r[1,:,:,:]
