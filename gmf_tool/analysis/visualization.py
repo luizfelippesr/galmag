70ff09b911ca1ce8a7a8738e33ad7750044b1365
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-10-14 20:53:36 -0300

    Adjustments to the plotting module.

--- gmf_tool/analysis/visualization.py
+++ gmf_tool/analysis/visualization.py
@@ -10,37 +10,37 @@ def std_setup():
                                       '#6a3d9a','#cab2d6',]
     P.rcParams['lines.linewidth'] = 3.0
 
-def plot_r_z_uniform(B,skipr=3,skipz=5):
+def plot_r_z_uniform(B,skipr=3,skipz=5, quiver=True, contour=True):
     """
     Plots a r-z slice of the field. Assumes B is created using a cylindrical
     grid - for a more sophisticated/flexible plotting script which does not
     rely on the grid structure check the plot_slice.
 
     The plot consists of:
-      1) a coloured contourplot of |B|^2
-      2) Field lines of the B_x and B_y field
-      3) Quivers showing the B_x and B_y field
+      1) a coloured contourplot of B_\phi
+      2) quivers showing the x-z projection of the field
 
     Input:
       B -> a B_field or B_field_component object
-      field_lines, quiver, contour -> booleans controlling which type of plot
+      quiver, contour -> booleans controlling which type of plot
                                       should be displayed. (Default: all True.)
-      skipx, skipy -> arguments to tweak the the displaying of the
-                      quivers. (Default: skipx=5, skipy=5)
+      skipx, skipz -> arguments to tweak the the displaying of the
+                      quivers. (Default: skipz=5, skipx=5)
     """
     # Requires a cylindrical grid
     assert B.grid.grid_type == 'cylindrical'
 
     # Makes a color contour plot
-    CP = P.contourf(B.grid.r_cylindrical[:,0,:], B.grid.z[:,0,:],
-                  -B.phi[:,0,:], alpha=0.5, linewidths=17.0)
-    CB = P.colorbar(CP, label=r'$B_\phi\,[\mu{{\rm G}}]$',)
-    P.setp(CP.collections , linewidth=2)
+    if contour:
+        CP = P.contourf(B.grid.r_cylindrical[:,0,:], B.grid.z[:,0,:],
+                      -B.phi[:,0,:], alpha=0.5, linewidths=17.0)
+        CB = P.colorbar(CP, label=r'$B_\phi\,[\mu{{\rm G}}]$',)
+        P.setp(CP.collections , linewidth=2)
 
-
-    P.quiver(B.grid.r_cylindrical[::skipr,0,::skipz], B.grid.z[::skipr,0,::skipz],
-           B.r_cylindrical[::skipr,0,::skipz],B.z[::skipr,0,::skipz],
-           color='0.25', alpha=0.75)
+    if quiver:
+        P.quiver(B.grid.r_cylindrical[::skipr,0,::skipz], B.grid.z[::skipr,0,::skipz],
+                 B.r_cylindrical[::skipr,0,::skipz],B.z[::skipr,0,::skipz],
+                 color='0.25', alpha=0.75)
 
     P.ylim([B.grid.z[:,0,:].min(),
           B.grid.z[:,0,:].max()])
@@ -51,35 +51,88 @@ def plot_r_z_uniform(B,skipr=3,skipz=5):
     P.ylabel(r'$z\,[{{\rm kpc}}]$')
 
 
-def plot_x_z_uniform(B,skipx=1,skipz=5):
+def plot_x_z_uniform(B,skipx=1,skipz=5,iy=0, quiver=True, contour=True):
     """
-    A wrapper to use plot_r_z_uniform, which is equivalent in terms of
-    indexing.
+    Plots a x-z slice of the field. Assumes B is created using a cartesian
+    grid - for a more sophisticated/flexible plotting script which does not
+    rely on the grid structure check the plot_slice.
+
+    The plot consists of:
+      1) a coloured contourplot of B_\phi
+      2) quivers showing the x-z projection of the field
+
+    Input:
+      B -> a B_field or B_field_component object
+      quiver, contour -> booleans controlling which type of plot
+                         should be displayed. (Default: all True.)
+      skipx, skipz -> arguments to tweak the the displaying of the
+                      quivers. (Default: skipz=5, skipx=5)
     """
     # Requires a Cartesian grid
     assert B.grid.grid_type == 'cartesian'
 
     # Makes a color contour plot
-    CP = P.contourf(B.grid.x[:,0,:], B.grid.z[:,0,:],
-                  -B.phi[:,0,:], alpha=0.5, linewidths=17.0)
-    CB = P.colorbar(CP, label=r'$B_\phi\,[\mu{{\rm G}}]$',)
-    P.setp(CP.collections , linewidth=2)
+    if contour:
+        CP = P.contourf(B.grid.x[:,iy,:], B.grid.z[:,iy,:],
+                      B.phi[:,iy,:], alpha=0.5, linewidths=17.0)
+        CB = P.colorbar(CP, label=r'$B_\phi\,[\mu{{\rm G}}]$',)
+        P.setp(CP.collections , linewidth=2)
 
+    if quiver:
+        P.quiver(B.grid.x[::skipx,iy,::skipz], B.grid.z[::skipx,iy,::skipz],
+                 B.x[::skipx,iy,::skipz],B.z[::skipx,iy,::skipz],
+                 color='0.25', alpha=0.75)
 
-    P.quiver(B.grid.x[::skipx,0,::skipz], B.grid.z[::skipx,0,::skipz],
-           B.x[::skipx,0,::skipz],B.z[::skipx,0,::skipz],
-           color='0.25', alpha=0.75)
-
-    P.ylim([B.grid.z[:,0,:].min(),
-          B.grid.z[:,0,:].max()])
-    P.xlim([B.grid.x[:,0,:].min(),
-          B.grid.x[:,0,:].max()])
+    P.ylim([B.grid.z[:,iy,:].min(),
+          B.grid.z[:,iy,:].max()])
+    P.xlim([B.grid.x[:,iy,:].min(),
+          B.grid.x[:,iy,:].max()])
 
     P.xlabel(r'$x\,[{{\rm kpc}}]$')
     P.ylabel(r'$z\,[{{\rm kpc}}]$')
 
 
-def plot_x_y_uniform(B,skipx=5, skipy=5, iz=0, field_lines=True, quiver=True,
+def plot_y_z_uniform(B, skipy=5, skipz=5, ix=0, quiver=True, contour=True):
+    """
+    Plots a y-z slice of the field. Assumes B is created using a cartesian
+    grid - for a more sophisticated/flexible plotting script which does not
+    rely on the grid structure check the plot_slice.
+
+    The plot consists of:
+      1) a coloured contourplot of B_\phi
+      2) Quivers showing the y-z projection of the field
+
+    Input:
+      B -> a B_field or B_field_component object
+      quiver, contour -> booleans controlling which type of plot
+                         should be displayed. (Default: all True.)
+      skipy, skipz -> arguments to tweak the the displaying of the
+                      quivers. (Default: skipz=5, skipy=5)
+    """
+    # Requires a Cartesian grid
+    assert B.grid.grid_type == 'cartesian'
+
+    # Makes a color contour plot
+    CP = P.contourf(B.grid.y[ix,:,:], B.grid.z[ix,:,:],
+                   B.phi[ix,:,:], alpha=0.5, linewidths=17.0)
+    CB = P.colorbar(CP, label=r'$B_\phi\,[\mu{{\rm G}}]$',)
+    P.setp(CP.collections , linewidth=2)
+
+    if quiver:
+        P.quiver(B.grid.y[ix,::skipy,::skipz], B.grid.z[ix,::skipy,::skipz],
+                 B.y[ix,::skipy,::skipz],B.z[ix,::skipy,::skipz],
+                 color='0.25', alpha=0.75)
+
+    P.ylim([B.grid.z[ix,:,:].min(),
+          B.grid.z[ix,:,:].max()])
+    P.xlim([B.grid.y[ix,:,:].min(),
+          B.grid.y[ix,:,:].max()])
+
+    P.xlabel(r'$y\,[{{\rm kpc}}]$')
+    P.ylabel(r'$z\,[{{\rm kpc}}]$')
+
+
+def plot_x_y_uniform(B, skipx=5, skipy=5, iz=0, field_lines=True, quiver=True,
                      contour=True):
     """
     Plots a x-y slice of the field. Assumes B is created using a cartesian
@@ -113,7 +166,7 @@ def plot_x_y_uniform(B,skipx=5, skipy=5, iz=0, field_lines=True, quiver=True,
                     -P.array(B.y[:,:,iz]), -P.array(B.x[:,:,iz]),color='r')
     if quiver:
         P.quiver(B.grid.x[::skipx,::skipy,iz], B.grid.y[::skipx,::skipy,iz],
-              B.x[::skipx,::skipy,iz],B.y[::skipx,::skipy,iz], color='b')
+              B.x[::skipx,::skipy,iz],B.y[::skipx,::skipy,iz], color='0.25')
 
     P.ylim([B.grid.y[:,:,iz].min(),B.grid.y[:,:,iz].max()])
     P.xlim([B.grid.x[:,:,iz].min(),B.grid.x[:,:,iz].max()])
