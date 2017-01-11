dddb05f8a4b37e29fc04d35764a7f2699d38ea72
Luiz Felippe S. Rodrigues <luiz.rodrigues@newcastle.ac.uk>
2017-01-11 15:47:04 +0000

    Updates setup.py with the new package name.

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import os, os.path
+exec(open('galmag/version.py').read())
 
-exec(open('gmf_tool/version.py').read())
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name="gmf_tool",
+setup(name="galmag",
       version=__version__,
-      author="Luiz Rodrigues <luiz.rodrigues@newcastle.ac.uk>, "
+      author="Luiz Felippe S. Rodrigues <luiz.rodrigues@newcastle.ac.uk>, "
              "Theo Steininger <theos@mpa-garching.mpg.de>",
       author_email="luiz.rodrigues@newcastle.ac.uk",
-      description="Galactic magnetic fields based on the dynamo equation",
-      url="",
-      packages=["gmf_tool", "gmf_tool.analysis", "gmf_tool.B_generators"],
-      license="",
+      keywords = "magnetic field, galaxy, Galaxy",
+      description= ("Generates realistic galactic magnetic field based on the "
+                    "dynamo equation"),
+      url="https://bitbucket.org/luizfelippe/galmag",
+      packages=["galmag", "galmag.analysis", "galmag.B_generators"],
+      license="GPLv3",
       zip_safe=False,
+      long_description=read('README.md'),
       dependency_links=[
         'git+https://gitlab.mpcdf.mpg.de/ift/d2o.git#egg=d2o'],
       install_requires=['d2o'],
