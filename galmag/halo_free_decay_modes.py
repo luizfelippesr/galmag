ed200a7094d3ca606d6f5b0597110bdb69441752
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2020-06-26 18:11:49 +0200

    Coding style update.

--- galmag/halo_free_decay_modes.py
+++ galmag/halo_free_decay_modes.py
@@ -22,15 +22,15 @@ of the halo of a galaxy.
 They should be accessed through the function :func:`get_mode`.
 """
 from scipy.special import j0, j1, jv, jn_zeros
-import numpy as N
+import numpy as np
 from sympy import besselj
 from mpmath import mp, findroot
 import os.path
-pi=N.pi
-cos = N.cos
-tan = N.tan
-sin = N.sin
-sqrt = N.sqrt
+pi=np.pi
+cos = np.cos
+tan = np.tan
+sin = np.sin
+sqrt = np.sqrt
 
 def get_B_a_1(r, theta, phi, C=0.346, k=pi):
     r"""
@@ -57,7 +57,7 @@ def get_B_a_1(r, theta, phi, C=0.346, k=pi):
         :math:`B_r`, :math:`B_\theta`, :math:`B_\phi`
     """
     # Computes radial component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     separator = r <=1
     Q[separator] = r[separator]**(-0.5)*jv(3.0/2.0,k*r[separator])
     Q[~separator] = r[~separator]**(-2.0)*jv(3.0/2.0,k)
@@ -66,7 +66,7 @@ def get_B_a_1(r, theta, phi, C=0.346, k=pi):
 
     # Computes polar component
     # X = d(rQ1)/dr
-    X = N.empty_like(r)
+    X = np.empty_like(r)
     y = k*r[separator]
     X[separator] = sqrt(2.0/pi)*(y**2*sin(y) - sin(y) +
                                  y*cos(y)) / (k**(-0.5)*y**2)
@@ -75,7 +75,7 @@ def get_B_a_1(r, theta, phi, C=0.346, k=pi):
     Btheta = C*(-sin(theta)/r)*X
 
     # Sets azimuthal component
-    Bphi   = N.zeros_like(r)
+    Bphi   = np.zeros_like(r)
 
     return Br, Btheta, Bphi
 
@@ -107,7 +107,7 @@ def get_B_a_2(r, theta, phi, C=0.250, k=5.763):
 
     # Computes radial component
     separator = r <=1.
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(7.0/2.0, k*r[separator])
     Q[~separator] = r[~separator]**(-4.0)*jv(7.0/2.0, k)
 
@@ -117,7 +117,7 @@ def get_B_a_2(r, theta, phi, C=0.250, k=5.763):
     # X = d(rQ1)/dr
     # r<=1, first define some auxiliary quantities
     y = k*r[separator]
-    X = N.empty_like(r)
+    X = np.empty_like(r)
 
     X[separator]  = k**(0.5)*y**(0.5)*(jv(2.5,y) - jv(4.5,y))/2.0 \
                     + 0.5*r[separator]**(-0.5)*jv(3.5,y)
@@ -128,7 +128,7 @@ def get_B_a_2(r, theta, phi, C=0.250, k=5.763):
     Btheta = C*(-sin(theta)/r)*(5.*(cos(theta))**2-1.)*X
 
     # Sets azimuthal component
-    Bphi   = N.zeros_like(r)
+    Bphi   = np.zeros_like(r)
 
     return Br, Btheta, Bphi
 
@@ -161,13 +161,13 @@ def get_B_a_3(r, theta, phi, C=3.445, k=5.763):
     separator = r <=1.
 
     # Sets radial component
-    Br = N.zeros_like(r)
+    Br = np.zeros_like(r)
 
     # Sets polar component
-    Btheta = N.zeros_like(r)
+    Btheta = np.zeros_like(r)
 
     # Computes azimuthal component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(5.0/2.0, k*r[separator])
     Q[~separator] = r[~separator]**(-3.0)*jv(5.0/2.0, k)
 
@@ -231,14 +231,14 @@ def get_B_s_1(r, theta, phi, C=0.653646562698, k=4.493409457909):
     separator = r <=1.
 
     # Computes radial component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(5.0/2.0,k*r[separator])
     Q[~separator]  = r[~separator]**(-3.0)*jv(5.0/2.0,k)
 
     Br = C*Q*(3.0*cos(theta)**2-1)/r
 
     # Computes theta component
-    X = N.zeros_like(r)
+    X = np.zeros_like(r)
     y = k*r[separator]
     siny = sin(y)
     cosy = cos(y)
@@ -251,7 +251,7 @@ def get_B_s_1(r, theta, phi, C=0.653646562698, k=4.493409457909):
     Btheta = C*(-sin(theta)*cos(theta)/r)*X
 
     # Sets azimuthal component
-    Bphi   = N.zeros_like(r)
+    Bphi   = np.zeros_like(r)
 
     return Br, Btheta, Bphi
 
@@ -284,13 +284,13 @@ def get_B_s_2(r, theta, phi, C=1.32984358196, k=4.493409457909):
     separator = r <=1.
 
     # Sets radial component
-    Br = N.zeros_like(r)
+    Br = np.zeros_like(r)
 
     # Sets theta component
-    Btheta = N.zeros_like(r)
+    Btheta = np.zeros_like(r)
 
     # Computes azimuthal component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(3.0/2.0, k*r[separator])
     Q[~separator] = r[~separator]**(-2.0)*jv(3.0/2.0, k)
 
@@ -330,7 +330,7 @@ def get_B_s_3(r, theta, phi, C=0.0169610298034, k=6.987932000501):
     y = k*r[separator]
 
     # Computes radial component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(9.0/2.0,y)
     Q[~separator]  = r[~separator]**(-5.0)*jv(9.0/2.0,k)
     S = -700.*cost**4+600.*cost**2-60
@@ -346,7 +346,7 @@ def get_B_s_3(r, theta, phi, C=0.0169610298034, k=6.987932000501):
     Btheta = -C * Q * S/r
 
     # Sets azimuthal component
-    Bphi = N.zeros_like(r)
+    Bphi = np.zeros_like(r)
 
     return Br, Btheta, Bphi
 
@@ -378,13 +378,13 @@ def get_B_s_4(r, theta, phi, C=0.539789362061, k=6.987932000501):
     separator = r <=1.
 
     # Sets radial component
-    Br = N.zeros_like(r)
+    Br = np.zeros_like(r)
 
     # Sets theta component
-    Btheta = N.zeros_like(r)
+    Btheta = np.zeros_like(r)
 
     # Computes azimuthal component
-    Q = N.empty_like(r)
+    Q = np.empty_like(r)
     Q[separator] = r[separator]**(-0.5)*jv(7.0/2.0, k*r[separator])
     Q[~separator] = r[~separator]**(-4.0)*jv(7.0/2.0, k)
     S = 3.*sin(theta)*(1.-5.*(cos(theta))**2)
@@ -481,8 +481,8 @@ class xi_lookup_table(object):
         if regenerate or (not os.path.isfile(filepath)):
             self.generate_xi_lookup_table(**kwargs)
         else:
-            self.table = N.load(filepath)
-            self.max_n, self.max_l = N.shape(self.table)
+            self.table = np.load(filepath)
+            self.max_n, self.max_l = np.shape(self.table)
 
 
     def get_xi(self, n, l, regenerate=False, **kwargs):
@@ -539,7 +539,7 @@ class xi_lookup_table(object):
 
         .. math::
             \gamma_{nl} = -(\xi_{nl})^2
-        
+
         The root are found using the mpmath.findroot function. The search
         for roots supplying findroot with number_of_guesses initial guesses
         uniformly distributed in the interval [3,max_guess].
@@ -560,7 +560,7 @@ class xi_lookup_table(object):
         numpy.ndarray
             A (max_n,max_l)-array containing containing the roots of
         """
-        self.table = N.empty((max_n,max_l))
+        self.table = np.empty((max_n,max_l))
         guesses = linspace(3,max_guess,number_of_guesses)
 
         for n in range(1,max_n+1):
@@ -571,16 +571,16 @@ class xi_lookup_table(object):
                 try:
 
                     # Stores every root found
-                    results.append(N.float64(findroot(f, guess)))
+                    results.append(np.float64(findroot(f, guess)))
                 except ValueError:
                     # Ignores failures in finding the root
                     pass
             # Excludes identical results
-            results = N.unique(results)
+            results = np.unique(results)
             # Avoids spurious results close to 0
             results = results[results>1.0]
             # Updates table
             self.table[n-1,:] = results[:max_l]
         if save:
-            N.save(self.filepath, self.table)
-        self.max_n, self.max_l = N.shape(self.table)
+            np.save(self.filepath, self.table)
+        self.max_n, self.max_l = np.shape(self.table)
